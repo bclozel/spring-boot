b54dc193cf4791c0c49618d6982f981710abf57e
Brian Clozel <bclozel@pivotal.io>
2016-06-01 22:00:39 +0200

    Polish

--- spring-boot/src/main/java/org/springframework/boot/context/embedded/jetty/JasperInitializer.java
+++ spring-boot/src/main/java/org/springframework/boot/context/embedded/jetty/JasperInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,7 @@ import org.springframework.util.ClassUtils;
  * @author Vladimir Tsanev
  */
 public class JasperInitializer extends AbstractLifeCycle {
+
 	private final WebAppContext context;
 	private final ServletContainerInitializer initializer;
 
